f3495da44950e78346ce034a148dbe095839e4b1
patl <patl@vmware.com>
2019-06-17 08:08:55 +0000

    add custom dictionary

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ use python 2.7 env, and look up 'world' for example:
 ```
 python cambrinary.py -w world
 ```
+
+# Help
+```
+python cambrinary.py --help
+```
--- cambrinary.py
+++ cambrinary.py
@@ -11,7 +11,7 @@ def load(word):
     return html
 
 
-def parse_header(dictionary):
+def parse_pronunciation(dictionary):
     """
     retrive the pronunciation for the word
     :param html: parsed_html by BeautifulSoup
@@ -42,52 +42,59 @@ def get_sense_block_title(sense_block):
         return pos.get_text() + ' [' + sub_word.get_text() + ']\n'
 
 
-def get_dictionary(html_obj):
+def get_dictionary(args):
     """
-    chose which specific dictionary would be used
-    :param html_obj:
-    :return:
-    """
+    chose which specific dictionary would be used:
     cambridge_advanced_learners_dictionary_and_thesaurus = 'cald4'
     cambridge_academic_content_dictionary = 'cacd'
-    return html_obj.body.find('div', attrs={'class': 'dictionary',
-                                            'data-id': cambridge_advanced_learners_dictionary_and_thesaurus})
+    :param args:
+    :return:
+    """
+    raw_html = load(args.word)
+    parsed_html = BeautifulSoup(raw_html, features='html.parser')
+    dictionary = parsed_html.body.find('div', attrs={'class': 'dictionary', 'data-id': args.dictionary})
+    if not dictionary:
+        print 'No result for ' + args.word
+        exit()
+    return dictionary
+
+
+def get_def_block(sense_block):
+    res = ''
+    block_title = get_sense_block_title(sense_block)
+    if block_title:
+        res += block_title
+    sense_body = sense_block.find('div', attrs={'class': 'sense-body'})
+    def_block_list = sense_body.findAll('div', attrs={'class': 'def-block pad-indent'})
+    for d in def_block_list:
+        definition = d.find('b', attrs={'class': 'def'})
+        examps = d.findAll('div', attrs={'class': 'examp emphasized'})
+        res += '* ' + definition.get_text() + '\n'
+        res += ''.join(['  - ' + e.get_text().strip() + '\n' for e in examps])
+    return res
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='A linux terminal dictionary based on cambridge online dictionary')
     parser.add_argument('-w', '--word', required=True, action='store', help='the word you want to look up')
+    parser.add_argument('-d', '--dictionary', action='store', default='cald4',
+                        help="to specific a dictiontry, defult(cambridge_advanced_learners_dictionary_and_thesaurus = 'cald4')")
     args = parser.parse_args()
     return args
 
 
 def main():
     args = get_args()
-    raw_html = load(args.word)
-    parsed_html = BeautifulSoup(raw_html, features='html.parser')
-    res = ''
-
-    dictionary = get_dictionary(parsed_html)
-    if not dictionary:
-        print 'No result for ' + args.word
-        return
+    dictionary = get_dictionary(args)
     # 'help' has verb and noun at the same time. So each of them for one entry_body_el
     entry_body_el_list = dictionary.findAll('div', attrs={'class': 'entry-body__el clrd js-share-holder'})
+    res = ''
     for el in entry_body_el_list:
-        pronunciation = parse_header(el)
+        pronunciation = parse_pronunciation(el)
         res += pronunciation
         sense_block_list = el.findAll('div', attrs={'class': 'sense-block'})  # one block for one meaning group.
-
         for i, block in enumerate(sense_block_list):
-            block_title = get_sense_block_title(block)
-            # check if the word has multi-sense block or just one
-            if block_title:  # word like 'get' having muti sense block
-                res += str(i + 1) + '. ' + block_title
-                def_list = block.findAll('b', attrs={'class': 'def'})
-                res += ''.join(['   - ' + d.get_text() + '\n' for d in def_list])
-            if not block_title:  # word like 'apple' having just one block
-                def_list = block.findAll('b', attrs={'class': 'def'})
-                res += ''.join([str(index + 1) + '. ' + d.get_text() + '\n' for index, d in enumerate(def_list)])
+            res += get_def_block(block)
     print res
 
 
